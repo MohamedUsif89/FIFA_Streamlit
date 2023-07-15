52d79ce836db6aeea14a373f1bb8d80bce7916dd
MohamedUsif89 <139069566+MohamedUsif89@users.noreply.github.com>
2023-07-15 16:12:28 +0300

    Add files via upload

---
+++ EDA_Fifa.py
@@ -0,0 +1,46 @@
+
+import numpy as np
+import pandas as pd
+import streamlit as st 
+import plotly.express as px
+
+st.title('FIFA Exploratory Data Analysis')
+
+st.markdown('This app is a data analysis of the FIFA datase')
+
+fifa = pd.read_csv('FIFA_.csv')
+
+st.subheader('Top 5 Valued players')
+
+
+
+#st.write(fifa.nlargest(5,'Value')[['Name', 'Value']])
+
+Top_player_Dict = fifa.nlargest(5,'Value')[['Name', 'Value']].set_index('Name').to_dict()
+
+#for name,value in Top_player_Dict['Value'].items():
+   # st.markdown(f"+ {name}: {value}")
+
+col1,col2  =st.columns(2)
+
+Histogram = px.histogram(fifa, x='Value', nbins=100)
+
+#Boxplot = px.box(fifa, x='Value', y='Name')
+top5 = fifa.nlargest(5, 'Value')[['Name', 'Value']]
+
+col1.write('#### Fifa Top Player')
+with col1:
+    Top_player_Dict = fifa.nlargest(5,'Value')[['Name', 'Value']].set_index('Name').to_dict()
+    for name,value in Top_player_Dict['Value'].items():
+        st.markdown(f"+ {name}: {value}")
+
+col2.write('#### Histogram Graph')
+with col2:
+    fig = px.bar(fifa.nlargest(5, 'Value') , x = 'Name', y = 'Value')
+    
+    st.plotly_chart(fig)
+
+#!pipreqs . 
+
+
+
